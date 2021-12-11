2b6573aa80d18cb50a221959b75bcd19f15457df
Brian Gates <brian.g.gates@gmail.com>
2021-12-11 10:13:49 -0800

    these were moved in the prior commit

--- src/commands/inspect/inspect.ts
+++ src/commands/inspect/inspect.ts
@@ -1,11 +1,5 @@
 import { SlashCommandBuilder } from "@discordjs/builders";
-import {
-  ChannelWebhookCreateOptions,
-  Collection,
-  CommandInteraction,
-  TextChannel,
-  Webhook,
-} from "discord.js";
+import { CommandInteraction, TextChannel } from "discord.js";
 import { getUserCharacter } from "../../character/getUserCharacter";
 import { characterEmbed } from "../../character/characterEmbed";
 import { questEmbed } from "../../quest/questEmbed";
@@ -15,6 +9,7 @@ import { values } from "remeda";
 import { statsEmbed } from "../../character/statsEmbed";
 import { itemEmbed } from "../../equipment/itemEmbed";
 import { Character } from "../../character/Character";
+import { getHook } from "./getHook";
 
 export const command = new SlashCommandBuilder()
   .setName("inspect")
@@ -48,32 +43,6 @@ export const execute = async (
   )
     inspectThread({ interaction, character });
 };
-
-type HookName = "Equipment" | "Status Effects" | "Quests";
-
-async function getHook({
-  name,
-  interaction,
-  webhooks,
-}: {
-  name: HookName;
-  interaction: CommandInteraction;
-  webhooks: Collection<string, Webhook>;
-}) {
-  const channel = interaction.channel;
-  if (!(channel instanceof TextChannel)) return;
-  const existingHook = webhooks.find((hook) => hook.name === name);
-  if (existingHook) return existingHook;
-  return await channel.createWebhook(name, hookOptions(name));
-}
-
-function hookOptions(name: HookName): ChannelWebhookCreateOptions {
-  return {
-    avatar:
-      "https://www.wallpaperup.com/uploads/wallpapers/2013/02/22/43066/33ee1c3920aa37d0b18a0de6cd9796b9.jpg",
-    reason: name,
-  };
-}
 
 async function inspectThread({
   interaction,
