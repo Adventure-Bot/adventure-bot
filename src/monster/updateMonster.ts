71db2a3200c3646f69172253bea82de50c2d2c5a
Brian Gates <brian.g.gates@gmail.com>
2021-12-28 12:50:51 -0800

    prune dead files and fix build

--- src/monster/updateMonster.ts
+++
@@ -1,9 +0,0 @@
-import { Monster } from "./Monster";
-import store from "../store";
-import { selectMonsterById } from "../store/selectors";
-import { updateCharacter } from "../character/updateCharacter";
-
-export const updateMonster = (monster: Monster): Monster => {
-  updateCharacter(monster);
-  return selectMonsterById(store.getState(), monster.id) ?? monster;
-};
