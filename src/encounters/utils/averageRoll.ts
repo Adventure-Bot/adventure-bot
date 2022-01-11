41f1a4794cf515b1506380b41897a73789e135e7
Brian Gates <brian.g.gates@gmail.com>
2022-01-10 22:54:31 -0800

    prune

--- src/encounters/utils/averageRoll.ts
+++
@@ -1,5 +0,0 @@
-import { AttackResult } from "../../attack/AttackResult";
-
-export const averageRoll = (attacks: AttackResult[]): number =>
-  attacks.reduce((total, attack) => total + attack.attackRoll, 0) /
-  attacks.length;
