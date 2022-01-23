4db9f47c53e941165177e407337c16184d1ac180
Brian Gates <brian.g.gates@gmail.com>
2022-01-22 18:13:04 -0800

    fix build

--- src/statusEffects/index.ts
+++ src/statusEffects/index.ts
@@ -1,7 +1,18 @@
-// todo: #97 statusEffects index
+import { StatModifier } from '@adventure-bot/statusEffects/StatModifier'
 import { StatusEffect } from '@adventure-bot/statusEffects/StatusEffect'
 import { createEffect } from '@adventure-bot/statusEffects/createEffect'
+import { hasStatusEffect } from '@adventure-bot/statusEffects/hasStatusEffect'
 import { statusEffectEmbed } from '@adventure-bot/statusEffects/statusEffectEmbed'
-import { EffectTemplate } from '@adventure-bot/statusEffects/templates'
+import { EffectTemplate, effects } from '@adventure-bot/statusEffects/templates'
+import { TemplateEffect } from '@adventure-bot/statusEffects/templates/TemplateEffect'
 
-export { createEffect, StatusEffect, statusEffectEmbed, EffectTemplate }
+export {
+  createEffect,
+  StatusEffect,
+  statusEffectEmbed,
+  EffectTemplate,
+  hasStatusEffect,
+  StatModifier,
+  effects,
+  TemplateEffect,
+}
