f267beec639cfa79a92d2f03709f17352f9b30b4
Jacob Kelley <jakie8@gmail.com>
2022-02-01 11:42:43 -0800

    chore(): move env files to packages directory

--- packages/web/src/pages/api/auth/[...nextauth].ts
+++ packages/web/src/pages/api/auth/[...nextauth].ts
@@ -1,14 +1,15 @@
 import NextAuth from 'next-auth'
-import DiscordProvider from 'next-auth/providers/discord'
+
+// import DiscordProvider from 'next-auth/providers/discord'
 
 export default NextAuth({
   // https://next-auth.js.org/configuration/options#secret
-  secret: 'Nz1gMvgUCxzX34uFsqrjyLnUoCSXADL5qj2YQxMxLhU=',
+  secret: 'change-me',
 
   providers: [
-    DiscordProvider({
-      clientId: process.env.DISCORD_CLIENT_ID,
-      clientSecret: process.env.DISCORD_CLIENT_SECRET,
-    }),
+    // DiscordProvider({
+    //   clientId: process.env.DISCORD_CLIENT_ID,
+    //   clientSecret: process.env.DISCORD_CLIENT_SECRET,
+    // }),
   ],
 })
