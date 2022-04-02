62e144d0bf371ba22c59eb241eb9ba0aeb07f169
Brian Gates <brian.g.gates@gmail.com>
2022-04-02 16:24:53 -0700

    trap refactoring

--- packages/game/src/encounters/trap/index.ts
+++ packages/game/src/encounters/trap/index.ts
@@ -12,32 +12,16 @@ import { updateUserQuestProgess } from '@adventure-bot/game/quest'
 import { trapAttack, trapRollText } from '@adventure-bot/game/trap'
 import {
   CommandHandlerOptions,
-  asset,
   sleep,
   weightedTable,
 } from '@adventure-bot/game/utils'
 
-type Trap = {
+import { traps } from './traps'
+
+export type Trap = {
   name: string
   image: string
   description: string
-}
-
-const traps: Record<string, () => Trap> = {
-  glyph: () => ({
-    name: 'glyph trap',
-    description: 'As your foot hits the ground, runes begin to glow!',
-    image: asset(
-      'fantasy',
-      'places',
-      'dungeon floor with glowing magical glyphs'
-    ).s3Url,
-  }),
-  ball: () => ({
-    name: 'stone ball trap',
-    description: 'A huge stone ball comes bowling down on you!',
-    image: 'https://imgur.com/TDMLxyE.png',
-  }),
 }
 
 const getRandomTrap = (): Trap => {
---
+++ packages/game/src/encounters/trap/traps.ts
@@ -0,0 +1,20 @@
+import { asset } from '@adventure-bot/game/utils'
+
+import { Trap } from '.'
+
+export const traps: Record<string, () => Trap> = {
+  glyph: () => ({
+    name: 'glyph trap',
+    description: 'As your foot hits the ground, runes begin to glow!',
+    image: asset(
+      'fantasy',
+      'places',
+      'dungeon floor with glowing magical glyphs'
+    ).s3Url,
+  }),
+  ball: () => ({
+    name: 'stone ball trap',
+    description: 'A huge stone ball comes bowling down on you!',
+    image: 'https://imgur.com/TDMLxyE.png',
+  }),
+}
