29b7bff3566795152b3a7f78993fa1e9142986b2
Brian Gates <brian.g.gates@gmail.com>
2022-04-15 23:01:18 -0700

    prune

--- packages/game/src/character/inventoryFields.ts
+++
@@ -1,9 +0,0 @@
-import { EmbedFieldData } from 'discord.js'
-
-import { Character } from '@adventure-bot/game/character'
-
-export const inventoryFields = (character: Character): EmbedFieldData[] =>
-  character.inventory.map((item) => ({
-    name: item.type,
-    value: item.name,
-  }))
