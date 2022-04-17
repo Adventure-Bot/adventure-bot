e2a853a8283b67004c60ca21e969fcfb92123741
Brian Gates <brian.g.gates@gmail.com>
2022-04-16 23:04:16 -0700

    improved encounter summaries

--- packages/game/src/encounters/encounterSummaryEmbed.ts
+++ packages/game/src/encounters/encounterSummaryEmbed.ts
@@ -30,27 +30,50 @@ export function encounterSummaryEmbed({
   embed.setTitle(
     `${decoratedName(character)} encountered ${decoratedName(monster)}!`
   )
+  embed.setTimestamp(new Date(encounter.date))
 
   switch (encounter.outcome) {
     case 'double ko':
+      embed.setTitle(
+        `${decoratedName(character)} and ${decoratedName(
+          monster
+        )} knocked each other out!`
+      )
       embed.addField('Double KO!', `They knocked eachother out!`)
       break
     case 'in progress':
       embed.addField('In Progress', 'Encounter in progress!')
       break
     case 'monster fled':
-      embed.addField('Evaded!', Emoji('run') + `${monster.name} escaped!`)
+      embed.setTitle(
+        `${decoratedName(monster)} fled from ${decoratedName(character)}!`
+      )
+      embed.addField(
+        'Evaded!',
+        Emoji('run') + `${decoratedName(monster)} escaped!`
+      )
       break
     case 'player defeated':
-      embed.addField('Unconscious', `${character.name} was knocked out!`)
+      embed.setTitle(
+        `${decoratedName(monster)} knocked ${decoratedName(character)} out!`
+      )
       break
     case 'player fled':
-      embed.addField('Fled', `${character.name} escaped with their life!`)
+      embed.setTitle(
+        `${decoratedName(character)} fled from ${decoratedName(monster)}!`
+      )
+      embed.addField(
+        'Fled',
+        `${decoratedName(character)} escaped with their life!`
+      )
       break
     case 'player victory':
+      embed.setTitle(
+        `${decoratedName(character)} defeated ${decoratedName(monster)}!`
+      )
       embed.addField(
         'Triumphant!',
-        `${character.name} defeated ${monster.name}! 🎉`
+        `${decoratedName(character)} defeated ${decoratedName(monster)}! 🎉`
       )
       embed.addFields([xpGainField(monster.xpValue)])
       break
