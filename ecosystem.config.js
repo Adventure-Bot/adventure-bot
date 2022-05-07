3308aa18cf44fd08798cb9d25f528b8796a60c89
Brian Gates <brian.g.gates@gmail.com>
2022-05-06 19:44:46 -0700

    reload this way instead

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,8 +1,10 @@
 module.exports = {
   apps: [
     {
-      name: 'game',
-      script: 'packages/game/.build/index.js',
+      name: 'Adventure Bot',
+      script: 'packages/game/src/index.ts',
+      watch: ['packages/game/src', 'packages/game/src/index.ts'],
+      ignore_watch: ['node_modules'],
     },
   ],
 }
