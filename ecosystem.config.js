558dc395c30d6640174aef0c6ba34aa0f240e85c
Brian Gates <brian.g.gates@gmail.com>
2022-05-08 00:02:51 -0700

    ignore .git

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,7 @@ module.exports = {
       args: 'game start',
       interpreter: '/bin/bash',
       watch: true,
-      ignore_watch: ['node_modules'],
+      ignore_watch: ['node_modules', '.git'],
     },
   ],
 }
