912a02530984b69affa473b81c441e343460f3b5
Brian Gates <brian.g.gates@gmail.com>
2022-05-08 21:09:55 -0700

    use built artifact

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,9 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'Adventure Bot',
-      script: 'yarn',
-      args: 'game start',
-      interpreter: '/bin/bash',
+      script: 'packages/game/.build',
     },
   ],
 }
