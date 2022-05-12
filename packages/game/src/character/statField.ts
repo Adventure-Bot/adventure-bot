6d8673324d848de879f7d7ed0e805f62194e6dac
Brian Gates <brian.g.gates@gmail.com>
2022-05-12 10:14:30 -0700

    refactor statText into its solitary dependant file

--- packages/game/src/character/statField.ts
+++ packages/game/src/character/statField.ts
@@ -1,10 +1,12 @@
 import { APIEmbedField } from 'discord-api-types'
 import { CommandInteraction } from 'discord.js'
 
+import { Emoji } from '@adventure-bot/game/Emoji'
 import {
   Character,
   Stat,
-  statText,
+  getCharacterStatModified,
+  getCharacterStatModifier,
   statTitles,
 } from '@adventure-bot/game/character'
 
@@ -18,4 +20,17 @@ export function statField(
     value: statText({ character, stat, interaction }),
     inline: true,
   }
+}
+function statText({
+  character,
+  stat,
+}: {
+  character: Character
+  stat: Stat
+  interaction: CommandInteraction
+}): string {
+  const modified = getCharacterStatModified(character, stat)
+  const modifier = getCharacterStatModifier(character, stat)
+  const sign = modifier > 0 ? '+' : ''
+  return Emoji(stat) + ` ${modified}${modifier ? ` (${sign}${modifier})` : ''}`
 }
