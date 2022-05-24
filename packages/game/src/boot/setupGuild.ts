8d45680ad786f10c28e12b4eab67319307b9aa43
Brian Gates <brian.g.gates@gmail.com>
2022-05-23 19:22:54 -0700

    install commands per guild

--- packages/game/src/boot/setupGuild.ts
+++ packages/game/src/boot/setupGuild.ts
@@ -1,5 +1,6 @@
 import { Client, Guild } from 'discord.js'
 
+import { installCommands } from '@adventure-bot/game/boot/installCommands'
 import { renderCharacterList } from '@adventure-bot/game/character'
 import { leaderboardChannel } from '@adventure-bot/game/leaderboard'
 
@@ -15,6 +16,14 @@ export async function setupGuild({
   renderCharacterList(guild)
   const appId = client.application?.id
   if (!appId) return
+
+  const { BOT_TOKEN, CLIENT_ID } = process.env
+  if (BOT_TOKEN && CLIENT_ID)
+    installCommands({
+      token: BOT_TOKEN,
+      clientId: CLIENT_ID,
+      guildId: guild.id,
+    })
   gameChannel({ guild, appId })
   leaderboardChannel({ guild, appId })
 }
