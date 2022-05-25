d526253322b466749b02bffed4f50d1dab11598d
Brian Gates <brian.g.gates@gmail.com>
2022-05-24 20:55:53 -0700

    hashes don't make sense with multiple guilds

--- packages/game/src/boot/installCommands.ts
+++ packages/game/src/boot/installCommands.ts
@@ -1,7 +1,5 @@
 import { REST } from '@discordjs/rest'
-import crypto from 'crypto'
 import { Routes } from 'discord-api-types/v9'
-import { readFile, writeFile } from 'fs/promises'
 
 import commands from '@adventure-bot/game/commands'
 
@@ -14,33 +12,21 @@ export async function installCommands({
   clientId: string
   guildId: string
 }): Promise<void> {
+  console.time('installCommands')
   const rest = new REST({ version: '9' }).setToken(token)
 
   try {
     const body = Array.from(commands.values()).map(({ command }) =>
       command.toJSON()
     )
-    const commandHash = crypto
-      .createHash('md5')
-      .update(JSON.stringify(body))
-      .digest('hex')
-    const priorHash = (
-      await readFile('.command-hash').catch(() => '')
-    ).toString()
-    if (commandHash === priorHash) {
-      console.log('✅ Commands are up-to-date')
-      return
-    }
-
     console.time('updating commands')
     await rest.put(Routes.applicationGuildCommands(clientId, guildId), {
       body,
     })
 
-    writeFile('.command-hash', commandHash)
-
     console.timeEnd('updating commands')
   } catch (error) {
     console.log(error)
   }
+  console.timeEnd('installCommands')
 }
