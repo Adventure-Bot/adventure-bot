98b7ca25401b602d5801218cbcb47ea93a6a1d37
Brian Gates <brian.g.gates@gmail.com>
2022-05-29 13:26:19 -0700

    try this

--- packages/game/src/env.ts
+++ packages/game/src/env.ts
@@ -14,10 +14,6 @@ export const assertEnv: () => void = () => {
       'CLIENT_ID',
       '\x1b[43m\x1b[30m ⚠ Discord Client ID required \n https://github.com/Adventure-Bot/adventure-bot/blob/main/developer-guide.md \x1b[0m',
     ],
-    [
-      'GUILD_ID',
-      '\x1b[43m\x1b[30m ⚠ Discord Guild ID required \n https://github.com/Adventure-Bot/adventure-bot/blob/main/developer-guide.md \x1b[0m',
-    ],
   ]
 
   let shouldExit = false
