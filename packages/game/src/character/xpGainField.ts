8bbd0b5e516a3720c8792a46091cf632d0fa70f2
Brian Gates <brian.g.gates@gmail.com>
2022-06-05 18:59:40 -0700

    -xpGainField

--- packages/game/src/character/xpGainField.ts
+++
@@ -1,8 +0,0 @@
-import { EmbedFieldData } from 'discord.js'
-
-import { Emoji } from '@adventure-bot/game/Emoji'
-
-export const xpGainField = (adjust = 0): EmbedFieldData => ({
-  name: `Experience Gained`,
-  value: Emoji('xp') + ' +' + adjust,
-})
