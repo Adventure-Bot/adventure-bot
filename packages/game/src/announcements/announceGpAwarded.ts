59cb7bc5b4a03e41c1213ec77f7fe321e9c86156
Brian Gates <brian.g.gates@gmail.com>
2022-06-08 23:09:15 -0700

    +announceGpAwarded

---
+++ packages/game/src/announcements/announceGpAwarded.ts
@@ -0,0 +1,30 @@
+import { MessageEmbed, TextChannel } from 'discord.js'
+
+import { EmojiValue } from '@adventure-bot/game/Emoji'
+import { decoratedName } from '@adventure-bot/game/character'
+import store from '@adventure-bot/game/store'
+import { startAppListening } from '@adventure-bot/game/store/listenerMiddleware'
+import { selectCharacterById } from '@adventure-bot/game/store/selectors'
+import { goldGained } from '@adventure-bot/game/store/slices/characters'
+
+export function announceGpAwarded({ channel }: { channel: TextChannel }): void {
+  startAppListening({
+    actionCreator: goldGained,
+    effect: ({ payload: { characterId, amount } }) => {
+      const character = selectCharacterById(store.getState(), characterId)
+      if (!character) return
+      const embeds = [
+        new MessageEmbed({
+          title: `${decoratedName(character)} gained ${EmojiValue(
+            'gold',
+            amount
+          )} gold!`,
+          color: 'YELLOW',
+        }),
+      ]
+      channel.send({
+        embeds,
+      })
+    },
+  })
+}
