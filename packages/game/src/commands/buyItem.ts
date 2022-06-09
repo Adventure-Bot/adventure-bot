74cac045285e98e365c52ea917d01322bffda884
Brian Gates <brian.g.gates@gmail.com>
2022-06-08 23:08:56 -0700

    prune

--- packages/game/src/commands/buyItem.ts
+++ packages/game/src/commands/buyItem.ts
@@ -1,6 +1,6 @@
 import { CommandInteraction } from 'discord.js'
 
-import { Character, adjustGold } from '@adventure-bot/game/character'
+import { Character } from '@adventure-bot/game/character'
 import { Item } from '@adventure-bot/game/equipment'
 import store from '@adventure-bot/game/store'
 import { itemPurchased } from '@adventure-bot/game/store/slices/shop'
