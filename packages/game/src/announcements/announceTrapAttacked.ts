51bf69555e4ff0c5e366fcbaa1fadc9f6084509f
Brian Gates <brian.g.gates@gmail.com>
2022-07-03 22:28:28 -0700

    refactor: trapAttack

--- packages/game/src/announcements/announceTrapAttacked.ts
+++ packages/game/src/announcements/announceTrapAttacked.ts
@@ -1,35 +1,18 @@
 import { MessageEmbed, TextChannel } from 'discord.js'
 
 import { Emoji } from '@adventure-bot/game/Emoji'
-import {
-  decoratedName,
-  getCharacter,
-  hpBarField,
-} from '@adventure-bot/game/character'
-import store from '@adventure-bot/game/store'
+import { sendEmbeds } from '@adventure-bot/game/announcements/sendEmbeds'
+import { decoratedName, hpBarField } from '@adventure-bot/game/character'
 import { trapAttacked } from '@adventure-bot/game/store/actions'
 import { startAppListening } from '@adventure-bot/game/store/listenerMiddleware'
-import { selectCharacterById } from '@adventure-bot/game/store/selectors'
-import {
-  damaged,
-  questProgressed,
-} from '@adventure-bot/game/store/slices/characters'
 import { trapRollText } from '@adventure-bot/game/trap'
 
 export function announceTrapAttacked(channel: TextChannel): void {
   startAppListening({
     actionCreator: trapAttacked,
-    effect: async ({ payload }) => {
+    effect: ({ payload }) => {
       const { messageId, result } = payload
-      const {
-        trap,
-        defender: { id: characterId },
-        outcome,
-        damage,
-      } = result
-      const state = store.getState()
-      const defender = selectCharacterById(state, characterId)
-      if (!defender) return
+      const { trap, defender, outcome, damage } = result
       const hitOrMissed = outcome === 'hit' ? 'hit' : 'missed'
       const embed = new MessageEmbed({
         title: `${Emoji(outcome)} ${decoratedName(
@@ -46,35 +29,11 @@ export function announceTrapAttacked(channel: TextChannel): void {
           hpBarField({ character: defender, adjustment: -damage }),
         ])
       }
-      const message = messageId
-        ? await channel.messages.fetch(messageId).catch(() => null)
-        : null
-
-      const embeds = [embed]
-      if (message) {
-        message.reply({ embeds })
-      } else {
-        channel.send({ embeds })
-      }
-
-      if ('hit' === outcome) {
-        if (damage) {
-          store.dispatch(
-            damaged({
-              character: defender,
-              amount: damage,
-            })
-          )
-          if (getCharacter(defender.id)?.hp ?? 0 > 0)
-            store.dispatch(
-              questProgressed({
-                characterId,
-                questId: 'survivor',
-                amount: damage,
-              })
-            )
-        }
-      }
+      sendEmbeds({
+        channel,
+        messageId,
+        embeds: [embed],
+      })
     },
   })
 }
