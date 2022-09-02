6182010bbb5b6b15c33a9c0d5cb746d8218304f4
Brian Gates <brian.g.gates@gmail.com>
2022-09-01 20:45:34 -0700

    updated travel art

--- packages/game/src/encounters/travel.ts
+++ packages/game/src/encounters/travel.ts
@@ -9,30 +9,20 @@ import { isTravelersRing } from '@adventure-bot/game/equipment/items/travelersRi
 import { createEffect } from '@adventure-bot/game/statusEffects'
 import store from '@adventure-bot/game/store'
 import { effectAdded } from '@adventure-bot/game/store/slices/statusEffects'
-import {
-  CommandHandlerOptions,
-  asset,
-  randomArrayElement,
-} from '@adventure-bot/game/utils'
+import { CommandHandlerOptions, asset } from '@adventure-bot/game/utils'
 
 export const travel = async ({
   interaction,
   replyType = 'editReply',
 }: CommandHandlerOptions): Promise<void> => {
   const character = findOrCreateCharacter(interaction.user)
-  const art = randomArrayElement([
-    asset('fantasy', 'places', 'a lone traveler in the desert'),
-    asset('fantasy', 'places', 'a lone traveler in the forest'),
-    asset('fantasy', 'places', 'a lone traveler in the mountains'),
-    asset('fantasy', 'places', 'a lone traveler in the plains'),
-  ])
   const message = await interaction[replyType]({
     embeds: [
       new MessageEmbed({
         title: `${decoratedName(character)} traveled.`,
         color: 'GREEN',
         description: `You travel the lands.`,
-      }).setImage(art.s3Url),
+      }).setImage(asset('fantasy', 'places', 'travel').s3Url),
     ],
   })
   awardXP({
