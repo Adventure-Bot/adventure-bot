4c04c5812c811a8700983445bee47d921cba02c3
Brian Gates <brian.g.gates@gmail.com>
2022-12-17 15:42:52 -0800

    add pyromancer monster, stats, art, names

---
+++ packages/game/src/monster/names/pyromancer.ts
@@ -0,0 +1,32 @@
+export const pyromancerNames = `
+Ignis
+Inferno
+Blaze
+Ember
+Pyra
+Scorch
+Solaris
+Volcanus
+Flamberge
+Cinder
+Firebrand
+Phoenix
+Magma
+Fyre
+Inferno Blaze
+Emberheart
+Cinderclaw
+Volcanix
+Pyritius
+Inferno Fury
+Sunfire
+Phoenix Flame
+Magma Storm
+Firestorm
+Flamewing
+Emberstar
+Volcanic Blaze
+Cinderfrost
+`
+  .split('\n')
+  .filter(Boolean)
