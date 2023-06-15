d4666c245b4aee7105246a1e98b5a58ee13050a6
Brian Gates <brian.g.gates@gmail.com>
2023-06-14 19:41:23 -0700

    prune and refactor

--- packages/game/src/quest/isQuestComplete.ts
+++ packages/game/src/quest/isQuestComplete.ts
@@ -1,12 +1,4 @@
-import { User } from 'discord.js'
-
-import { findOrCreateCharacter } from '@adventure-bot/game/character'
-import { Quest, QuestId } from '@adventure-bot/game/quest'
+import { Quest } from '@adventure-bot/game/quest'
 
 export const isQuestComplete = (quest: Quest): boolean =>
   quest.progress >= quest.totalRequired
-
-export const isUserQuestComplete = (user: User, questId: QuestId): boolean => {
-  const quest = findOrCreateCharacter(user).quests[questId]
-  return quest ? isQuestComplete(quest) : false
-}
