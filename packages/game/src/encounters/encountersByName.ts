064b92a272744966f28656da97a173b47d56f453
Brian Gates <brian.g.gates@gmail.com>
2023-07-08 16:58:33 -0700

    thugs

--- packages/game/src/encounters/encountersByName.ts
+++ packages/game/src/encounters/encountersByName.ts
@@ -16,6 +16,7 @@ import {
   vigorShrine,
 } from '@adventure-bot/game/encounters/shrine'
 import { tavern } from '@adventure-bot/game/encounters/tavern/tavern'
+import { thugs } from '@adventure-bot/game/encounters/thugs'
 import { townSquare } from '@adventure-bot/game/encounters/townsquare'
 import { trap } from '@adventure-bot/game/encounters/trap'
 import { travel } from '@adventure-bot/game/encounters/travel'
@@ -41,6 +42,7 @@ export const encountersByName: [string, CommandHandler][] = [
   ["Rogue's Guild", roguesGuild],
   ['Tavern', tavern],
   ['Town Square', townSquare],
+  ['Thugs', thugs],
   ['Trap', trap],
   ['Travel', travel],
 ]
