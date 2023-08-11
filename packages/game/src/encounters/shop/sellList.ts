7acb77f4960bd1ea1fb9281e61f65042f8d2c46f
Brian Gates <brian.g.gates@gmail.com>
2023-08-10 17:19:33 -0700

    sell list shows whether item is equipped

--- packages/game/src/encounters/shop/sellList.ts
+++ packages/game/src/encounters/shop/sellList.ts
@@ -1,20 +1,22 @@
-import { CommandInteraction, StringSelectMenuBuilder } from 'discord.js'
+import { StringSelectMenuBuilder } from 'discord.js'
 
 import { sellValue } from '@adventure-bot/game/encounters/shop/sellValue'
-import { Item } from '@adventure-bot/game/equipment'
+import { Item, isEquipped } from '@adventure-bot/game/equipment'
+import { CharacterWithStats } from '@adventure-bot/game/store/selectors'
 import { truncate } from '@adventure-bot/game/utils'
 
 export function sellList({
   inventory,
+  character,
 }: {
   inventory: Item[]
-  interaction: CommandInteraction
+  character: CharacterWithStats
 }): StringSelectMenuBuilder {
   return new StringSelectMenuBuilder({
     customId: 'item',
     placeholder: `Which item would you like to sell?`,
     options: inventory.slice(0, 24).map((item) => ({
-      label: item.name,
+      label: item.name + (isEquipped({ item, character }) ? ' (equipped)' : ''),
       description: truncate(`${sellValue(item)}g ${item.description}`, 100),
       value: item.id,
     })),
